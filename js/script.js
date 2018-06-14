839a6e25535540320055ba17a6adb9618abea5e4
Diane de Lassence <dianedelassence@gmail.com>
2018-06-14 16:14:53 +0200

    done

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ function Skieur () {
   this.height = 60;
   this.x = (myCanvas.width/2) - (this.width/2);
   this.y = myCanvas.height/4;
+  this.counter = counter;
 }
 
 Skieur.prototype.draw = function () {
@@ -27,8 +28,10 @@ function Obstacle (myImage, myWidth, myHeight) {
   this.speed = 3;
 }
 
-Obstacle.prototype.draw = function () {
-  this.y -= this.speed;
+Obstacle.prototype.draw = function (isCollision) {
+  if (!isCollision) {
+    this.y -= this.speed;
+   }
   ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
 };
 
@@ -47,8 +50,8 @@ function boxCollision (x1, y1, width1, height1, x2, y2, width2, height2) {
 var myCanvas = document.querySelector("canvas");
 
 // make the canvas take up the entire screen
-myCanvas.width = window.innerWidth;
-myCanvas.height = window.innerHeight;
+myCanvas.width = "800";
+myCanvas.height = "600";
 
 window.onresize = function () {
   // when the window is resized, resize the canvas
@@ -65,8 +68,8 @@ playerImg.src = "./img/skiMan.jpg";
 var pineImg = new Image();
 pineImg.src = "./img/pineTree.jpg";
 
-// var pizzaImg = new Image();
-// pizzaImg.src = "./images/pizza.png";
+var badManImg = new Image();
+badManImg.src = "./images/snowball.png";
 
 // var broccoliImg = new Image();
 // broccoliImg.src = "./images/broccoli.png";
@@ -92,12 +95,16 @@ var addObstacle = setInterval(function () {
 
   var newObstacle = new Obstacle(image, 35, 35);
   // if (image === broccoliImg || image === eggplantImg) {
-  //   newObstacle.isGood = false;
+  //   newObstacle.isGo od = false;
   // }
   movingObstacles.push(newObstacle);
 }, 200);
 
 var skieur = new Skieur();
+
+var counter = 600;
+// if (counter = -200){
+// this.speed = 0 };
 
 var drawLoop = setInterval(function () {
   // erase the old drawings
@@ -106,32 +113,34 @@ var drawLoop = setInterval(function () {
   // redraw everything
   skieur.draw();
 
-  // var affiche = window.prompt("brrrrr!!");
-  // affiche.draw();
+  var globalCollision = false;
 
   movingObstacles.forEach(function (oneObstacle) {
     // check collision with player
     var isCollision = boxCollision(skieur.x, skieur.y, skieur.width, skieur.height, oneObstacle.x, oneObstacle.y, oneObstacle.width, oneObstacle.height);
     if (isCollision) {
-      // clearInterval(drawLoop) && affiche;
+      globalCollision = true;
+      counter -= 1;
+      if (counter <=0) {
+        clearInterval( addObstacle );
+        movingObstacles.forEach( function (oneObstacle ) {
+          oneObstacle.speed = 0;
+        });
+      // ctx.drawImage (badManImg, myCanvas.width/2, myCanvas.height/2)
+      }
 
-        // stop.movingObstacles();
       // continue if it's a good obstacle
-      console.log( "OUCH")
+      console.log( "Aille");
+      console.log(counter);
       // if (oneObstacle.isGood) {
-      //   oneObstacle.isEaten = true;
     }
-    else {
-      oneObstacle.draw();
-    //   // GAME OVER if it's a bad obstacle
-    //   // (stop the loops to freeze the game)
-    //   clearInterval(drawLoop);
-    //   clearInterval(addObstacle);
-    // }
-    }
-    oneObstacle.draw();
   });
-
+  movingObstacles.forEach(function (oneObstacle) {
+    oneObstacle.draw(globalCollision);
+  });
+  // if (counter == 0){
+  //   alert("Game Over!!!")
+  // }
   // remove eaten obstacles from the array
   // movingObstacles = movingObstacles.filter(function (oneObstacle) {
   //   return !oneObstacle.isEaten;
@@ -172,3 +181,124 @@ body.onkeydown = function (event) {
       break;
   }
 };
+
+//----------------------------------------------------------------
+//-----------BACKGROUND SNOW EFFECT------------------------------
+//------------------------------------------------------------------
+
+// (function() {
+//   var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
+//   function(callback) {
+//       window.setTimeout(callback, 1000 / 60);
+//   };
+//   window.requestAnimationFrame = requestAnimationFrame;
+// })();
+
+
+// var flakes = [],
+//   canvas = document.getElementById("canvas"),
+//   ctx = canvas.getContext("2d"),
+//   flakeCount = 400,
+//   mX = -100,
+//   mY = -100
+
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
+
+// function snow() {
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//   for (var i = 0; i < flakeCount; i++) {
+//       var flake = flakes[i],
+//           x = mX,
+//           y = mY,
+//           minDist = 150,
+//           x2 = flake.x,
+//           y2 = flake.y;
+
+//       var dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y)),
+//           dx = x2 - x,
+//           dy = y2 - y;
+
+//       if (dist < minDist) {
+//           var force = minDist / (dist * dist),
+//               xcomp = (x - x2) / dist,
+//               ycomp = (y - y2) / dist,
+//               deltaV = force / 2;
+
+//           flake.velX -= deltaV * xcomp;
+//           flake.velY -= deltaV * ycomp;
+
+//       } else {
+//           flake.velX *= .98;
+//           if (flake.velY <= flake.speed) {
+//               flake.velY = flake.speed
+//           }
+//           flake.velX += Math.cos(flake.step += .05) * flake.stepSize;
+//       }
+
+//       ctx.fillStyle = "rgba(255,255,255," + flake.opacity + ")";
+//       flake.y += flake.velY;
+//       flake.x += flake.velX;
+
+//       if (flake.y >= canvas.height || flake.y <= 0) {
+//           reset(flake);
+//       }
+
+
+//       if (flake.x >= canvas.width || flake.x <= 0) {
+//           reset(flake);
+//       }
+
+//       ctx.beginPath();
+//       ctx.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
+//       ctx.fill();
+//   }
+//   requestAnimationFrame(snow);
+// };
+
+// function reset(flake) {
+//   flake.x = Math.floor(Math.random() * canvas.width);
+//   flake.y = 0;
+//   flake.size = (Math.random() * 3) + 2;
+//   flake.speed = (Math.random() * 1) + 0.5;
+//   flake.velY = flake.speed;
+//   flake.velX = 0;
+//   flake.opacity = (Math.random() * 0.5) + 0.3;
+// }
+
+// function init() {
+//   for (var i = 0; i < flakeCount; i++) {
+//       var x = Math.floor(Math.random() * canvas.width),
+//           y = Math.floor(Math.random() * canvas.height),
+//           size = (Math.random() * 3) + 2,
+//           speed = (Math.random() * 1) + 0.5,
+//           opacity = (Math.random() * 0.5) + 0.3;
+
+//       flakes.push({
+//           speed: speed,
+//           velY: speed,
+//           velX: 0,
+//           x: x,
+//           y: y,
+//           size: size,
+//           stepSize: (Math.random()) / 30,
+//           step: 0,
+//           opacity: opacity
+//       });
+//   }
+
+//   snow();
+// };
+
+// canvas.addEventListener("mousemove", function(e) {
+//   mX = e.clientX,
+//   mY = e.clientY
+// });
+
+// window.addEventListener("resize",function(){
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
+// })
+
+// init();
